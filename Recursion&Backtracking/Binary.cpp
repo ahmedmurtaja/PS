5ba0bd2d913cf7c04f32efde286550e0aec47e8c
ahmedmurtaja <ahmed.izz.murtaja@gmail.com>
2024-10-12 14:02:24 +0300

    Binary

--- Recursion&Backtracking/Binary.cpp
+++ Recursion&Backtracking/Binary.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 int n;
-void generatePossibleBinOfSize2n(string s)
+void generatePossibleBinOfSize2n(string s , int sum1 , int sum2)
 {
     if (s.size() == 2*n)
     {
+        if(sum1 == sum2)
         cout << s << endl;
         return;
     }
-    //  TRY '0'
-    generatePossibleBinOfSize2n(s + '0');
-    //  TRY '1'
-    generatePossibleBinOfSize2n(s + '1');
+    //  TRY '0'       N
+    generatePossibleBinOfSize2n(s + '0', sum1 , sum2);
+    //  TRY '1' console.log(s);
+    
+    if(s.size()< n) generatePossibleBinOfSize2n(s + '1' , sum1+1 , sum2);
+    else generatePossibleBinOfSize2n(s + '1' , sum1 , sum2+1);
     // this will generate the tree of 0s & 1s
 }
 
@@ -38,7 +41,7 @@ int main()
     // Given a number n, find all binary Sequences of length 2n such that the sum  
     // of the first n bits is the same as sum of last n bits
 cin >> n;
-// generatePossibleBinOfSize2n("");
-possibleBinOfSize2n("", 0, 0);
+generatePossibleBinOfSize2n("", 0 , 0);
+// possibleBinOfSize2n("", 0, 0);
 
 }
